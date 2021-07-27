0ad0100ca538ee901ce74e17d3dd2611888c6809
dracero <diego.racero@gmail.com>
2021-07-26 21:07:54 -0300

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// Config starter code
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  initialMessages: [createChatBotMessage(`Bienvenido al chatbot de Física I`)]
+};
+
+export default config;
